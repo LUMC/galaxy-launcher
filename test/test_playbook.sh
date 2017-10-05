05cd081eefb7ae34eb900e67e499e06c9eb14bce
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-05 11:58:04 +0200

    added upgrade test variables

--- test/test_playbook.sh
+++ test/test_playbook.sh
@@ -10,21 +10,25 @@ ssh_user="galaxy_ssh"
 
 export_volume="$project_root/test/CI/files/$hostname"
 export_folder="$export_volume/export"
+vars_file=$export_volume/vars_file.yml
+
 ansible_playbook_extra_settings="\
-galaxy_docker_container_name=galaxy_${hostname} \
-galaxy_docker_extract_database_dir=${project_root}/test/CI/files/{{inventory_hostname}}/import_db \
-galaxy_docker_import_db_dir=${project_root}/test/CI/files/{{inventory_hostname}}/import_db \
-galaxy_docker_export_location=${export_folder} \
-galaxy_docker_web_port_public=8081 \
-galaxy_docker_web_port=8080 \
-galaxy_docker_ansible_generated_vars_dir=${project_root}/test/CI/files/{{inventory_hostname}} \
-galaxy_docker_upgrade_test_settings='{\
-\"galaxy_docker_docker_image\":\"bgruening/galaxy-stable:17.05\",\
-\"galaxy_docker_container_name\":\"test_upgrade_galaxy\",\
-\"galaxy_docker_web_port\":\"8888\",\
-\"galaxy_docker_web_port_public\":\"8880\",\
-\"galaxy_docker_sftp_port\":\"8222\"\
-}'\
+galaxy_docker_container_name: galaxy_${hostname}
+galaxy_docker_extract_database_dir: ${export_volume}/import_db
+galaxy_docker_import_db_dir: ${export_volume}/import_db
+galaxy_docker_export_location: ${export_folder}
+galaxy_docker_web_port_public: 8081
+galaxy_docker_web_port: 8080
+galaxy_docker_ansible_generated_vars_dir: ${export_volume}
+galaxy_docker_upgrade_test_settings:
+  galaxy_docker_docker_image: 'bgruening/galaxy-stable:17.05'
+  galaxy_docker_container_name: test_upgrade_galaxy
+  galaxy_docker_export_location: ${export_volume}/test_upgrade
+  galaxy_docker_ufw_profile: test_upgrade_galaxy
+  galaxy_docker_web_port: 8888
+  galaxy_docker_web_port_public: 8880
+  galaxy_docker_sftp_port: 8822
+  galaxy_brand: Test upgrade of Galaxy to 17.05
 "
 echo $ansible_playbook_extra_settings
 ansible_playbook_run_commands="\
@@ -52,7 +56,6 @@ mkdir -p $export_volume
 docker run -v $project_root/test/CI/files:$project_root/test/CI/files $image_name \
 chmod 777 $export_volume
 
-
 echo "start docker container"
 CONTAINER_NAME=`docker run -d \
 --cap-add=NET_ADMIN \
@@ -75,13 +78,15 @@ ansible_ssh_private_key_file=$project_root/test/docker/$hostname/files/$ssh_user
 echo "[all:vars]" >> $hosts_file
 echo 'ansible_ssh_extra_args="-o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"' >> $hosts_file
 
+echo "create vars file"
+echo "$ansible_playbook_extra_settings" > $vars_file
 
 echo "Run playbook to install prerequisites"
 ansible-playbook -i $hosts_file main.yml \
 -e "host=$hostname \
-${ansible_playbook_extra_settings} \
 run=install_prerequisites \
-galaxy_docker_create_user_ssh_keys=true"
+galaxy_docker_create_user_ssh_keys=true" \
+--extra-vars @$vars_file
 
 echo "Run playbook run commands"
 for run_command in $ansible_playbook_run_commands
@@ -89,9 +94,9 @@ do
   echo "Running: ${run_command}"
   ansible-playbook -i $hosts_file main.yml \
   -e "host=$hostname \
-  ${ansible_playbook_extra_settings} \
   run=${run_command} \
-  galaxy_docker_run_privileged=$privileged"
+  galaxy_docker_run_privileged=$privileged" \
+  --extra-vars @$vars_file
 done
 
 echo "Remove directories"
