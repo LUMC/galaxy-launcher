134b3f54502a39502249d79b59834805f88aa228
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-06 11:39:12 +0200

    added cluster-test

--- test/test_playbook.sh
+++ test/test_playbook.sh
@@ -13,39 +13,7 @@ export_volume="$project_root/test/CI/files/$hostname"
 export_folder="$export_volume/export"
 vars_file=$export_volume/vars_file.yml
 
-ansible_playbook_extra_settings="\
-galaxy_docker_container_name: galaxy_${hostname}
-galaxy_docker_extract_database_dir: ${export_volume}/import_db/
-galaxy_docker_import_db_dir: ${export_volume}/import_db/
-galaxy_docker_export_location: ${export_folder}
-galaxy_docker_web_port_public: 8081
-galaxy_docker_web_port: 8080
-galaxy_docker_ansible_generated_vars_dir: ${export_volume}
-galaxy_docker_empty_database_script: ${export_volume}/new_empty_db
-galaxy_docker_imported_db_location: ${export_volume}/database/
-galaxy_docker_upgrade_test_settings:
-  galaxy_docker_docker_image: 'bgruening/galaxy-stable:17.05'
-  galaxy_docker_container_name: test_upgrade_galaxy
-  galaxy_docker_export_location: ${export_volume}/test_upgrade
-  galaxy_docker_ufw_profile: test_upgrade_galaxy
-  galaxy_docker_web_port: 8888
-  galaxy_docker_web_port_public: 8880
-  galaxy_docker_sftp_port: 8823
-  galaxy_brand: Test upgrade of Galaxy to 17.05
-"
-echo $ansible_playbook_extra_settings
-ansible_playbook_run_commands="\
-install_galaxy \
-['install_tools','install_genomes'] \
-enable_ldap \
-cron_database_backup \
-extract_database \
-import_database \
-upgrade_test \
-delete_upgrade_test \
-upgrade \
-delete_galaxy_complete \
-"
+source $project_root/test/variables/${hostname}.sh
 
 echo "Build docker image with ssh access"
 docker build -t $image_name $project_root/test/docker/$hostname
@@ -86,7 +54,6 @@ echo 'ansible_ssh_extra_args="-o IdentitiesOnly=yes -o StrictHostKeyChecking=no 
 echo "create vars file"
 echo "$ansible_playbook_extra_settings" > $vars_file
 
-echo "\$TRAVIS: $TRAVIS"
 if [[ -v TRAVIS ]]
 then
   echo "Changing ansible temp directories for travis"
