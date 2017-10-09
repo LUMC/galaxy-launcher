012a70680eb082f6783ab28ff7e59f6ab2291fd0
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-09 15:00:23 +0200

    information about excluded files

--- test/ansiblelinting.sh
+++ test/ansiblelinting.sh
@@ -5,6 +5,8 @@ set -o pipefail
 project_root=$(git rev-parse --show-toplevel)
 
 echo "ansible-linting of main.yml"
+# Excluded files contain !unsafe tag which causes ansible-lint to crash.
+# Issue has been filed https://github.com/willthames/ansible-lint/issues/291
 ansible-lint -p \
 --exclude=$project_root/roles/galaxy-docker/tasks/delete/delete_export_folder.yml \
 --exclude=$project_root/roles/galaxy-docker/tasks/prepare_docker/create_export_directory_folders.yml \
