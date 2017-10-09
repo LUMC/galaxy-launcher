35d6cb34e10a69569d550a92e2ce25ea79007305
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-09 16:32:58 +0200

    more elegant ansible-lint test

--- test/ansiblelinting.sh
+++ test/ansiblelinting.sh
@@ -4,11 +4,17 @@ set -o pipefail
 
 project_root=$(git rev-parse --show-toplevel)
 
-echo "ansible-linting of main.yml"
+
 # Excluded files contain !unsafe tag which causes ansible-lint to crash.
 # Issue has been filed https://github.com/willthames/ansible-lint/issues/291
-ansible-lint -p \
---exclude=$project_root/roles/galaxy-docker/tasks/delete/delete_export_folder.yml \
---exclude=$project_root/roles/galaxy-docker/tasks/prepare_docker/create_export_directory_folders.yml \
---exclude=$project_root/roles/galaxy-docker/tasks/start_container/install_python_dependencies.yml \
-$project_root/main.yml
+excluded_files="
+--exclude=$project_root/roles/galaxy-docker/tasks/delete/delete_export_folder.yml
+--exclude=$project_root/roles/galaxy-docker/tasks/prepare_docker/create_export_directory_folders.yml
+--exclude=$project_root/roles/galaxy-docker/tasks/start_container/install_python_dependencies.yml
+"
+
+echo "Check for ansible best practices using ansible-lint"
+for lint_files in $project_root/roles/galaxy-docker $project_root/roles/installdocker $project_root/main.yml
+do
+  ansible-lint -p $excluded_files $lint_files
+done
