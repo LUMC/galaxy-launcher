f548b6d00f097f8f35212277a606939286bd7f22
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-05-24 17:01:11 +0200

    upgrade version to 18.05

--- test/variables/cluster-test.sh
+++ test/variables/cluster-test.sh
@@ -36,20 +36,20 @@ galaxy_docker_empty_database_script: ${export_volume}/new_empty_db
 galaxy_docker_imported_db_location: ${export_volume}/database/
 galaxy_docker_container_hostname: $galaxy_hostname
 galaxy_docker_upgrade_test_settings:
-  galaxy_docker_docker_image: 'cluster-galaxy-stable:18.01'
+  galaxy_docker_docker_image: 'cluster-galaxy-stable:18.05'
   galaxy_docker_container_name: test_upgrade_galaxy
   galaxy_docker_shared_cluster_directory: ${export_volume}/test_upgrade
   galaxy_docker_ufw_profile: test_upgrade_galaxy
   galaxy_docker_web_port: 8888
   galaxy_docker_web_port_public: 8880
   galaxy_docker_sftp_port: 8823
-  galaxy_brand: Test upgrade of Galaxy to 18.01
+  galaxy_brand: Test upgrade of Galaxy to 18.05
 
 galaxy_docker_cluster_sge_parallel_environment: batch
 galaxy_docker_gridengine_master_host: $CLUSTER_NAME
 galaxy_docker_custom_image_lines:
   - run sed -i '3iecho \"$CLUSTER_IP $CLUSTER_NAME\" >> /etc/hosts' /usr/bin/startup
-bgruening_galaxy_stable_version: 18.01
+bgruening_galaxy_stable_version: 18.05
 "
 ansible_playbook_run_commands="\
 install_galaxy_cluster \
--- test/variables/standalone_vms.sh
+++ test/variables/standalone_vms.sh
@@ -11,14 +11,14 @@ galaxy_docker_ansible_generated_vars_dir: ${export_volume}
 galaxy_docker_empty_database_script: ${export_volume}/new_empty_db
 galaxy_docker_imported_db_location: ${export_volume}/database/
 galaxy_docker_upgrade_test_settings:
-  galaxy_docker_docker_image: 'bgruening/galaxy-stable:18.01'
+  galaxy_docker_docker_image: 'bgruening/galaxy-stable:18.05'
   galaxy_docker_container_name: test_upgrade_galaxy
   galaxy_docker_export_location: ${export_volume}/test_upgrade
   galaxy_docker_ufw_profile: test_upgrade_galaxy
   galaxy_docker_web_port: 8888
   galaxy_docker_web_port_public: 8880
   galaxy_docker_sftp_port: 8823
-  galaxy_brand: Test upgrade of Galaxy to 18.01
+  galaxy_brand: Test upgrade of Galaxy to 18.05
 "
 ansible_playbook_run_commands="\
 install_galaxy \
